5967f57d2282b3f26c6356de501bec52b5ab58eb
Alyssa Kwan <me@alyssackwan.name>
2016-06-14 05:23:45 +0000

    refactor to controllers

---
+++ src/controllers/carts.js
@@ -0,0 +1,47 @@
+var Cart = require('../models/cart');
+
+exports.create = function(req, res) {
+  res.json(new Cart());
+};
+
+exports.read = function(req, res) {
+  var cartId = req.param('cart_id');
+  var cart;
+  try {
+    cart = Cart.get(cartId);
+  }
+  catch (e) {
+    res.status(404).send('cart_id ' + cartId + ' not found');
+    return;
+  }
+  res.json(cart);
+};
+
+exports.createItems = function(req, res) {
+  var cartId = req.param('cart_id');
+  var cart;
+  try {
+    cart = Cart.get(cartId);
+  }
+  catch (e) {
+    res.status(404).send('cart_id ' + cartId + ' not found');
+    return;
+  }
+  var productId = req.body.productId;
+  var quantity = req.body.quantity;
+  if (!productId) {
+    res.status(400).send('invalid body: productId');
+    return;
+  }
+  if (!quantity) {
+    res.status(400).send('invalid body: quantity');
+    return;
+  }
+  cart.addItems(productId, quantity);
+  res.json(cart);
+};
+
+exports.createPurchase = function(req, res) {
+  /* Fill in the blank! */
+  res.json(null);
+};
--- src/server.js
+++ src/server.js
@@ -1,56 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser")
 
-var Cart = require('./models/cart');
+var cartController = require('./controllers/carts');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/carts', function(req, res) {
-  res.json(new Cart());
-});
-
-app.get('/carts/:cart_id', function(req, res) {
-  var cartId = req.param('cart_id');
-  var cart;
-  try {
-    cart = Cart.get(cartId);
-  }
-  catch (e) {
-    res.status(404).send('cart_id ' + cartId + ' not found');
-    return;
-  }
-  res.json(cart);
-});
-
-app.post('/carts/:cart_id/items', function(req, res) {
-  var cartId = req.param('cart_id');
-  var cart;
-  try {
-    cart = Cart.get(cartId);
-  }
-  catch (e) {
-    res.status(404).send('cart_id ' + cartId + ' not found');
-    return;
-  }
-  var productId = req.body.productId;
-  var quantity = req.body.quantity;
-  if (!productId) {
-    res.status(400).send('invalid body: productId');
-    return;
-  }
-  if (!quantity) {
-    res.status(400).send('invalid body: quantity');
-    return;
-  }
-  cart.addItems(productId, quantity);
-  res.json(cart);
-});
-
-app.post('/carts/:cart_id/purchases', function(req, res) {
-  /* Fill in the blank! */
-});
+app.post('/carts', cartController.create);
+app.get('/carts/:cart_id', cartController.read);
+app.post('/carts/:cart_id/items', cartController.createItems);
+app.post('/carts/:cart_id/purchases', cartController.createPurchase);
 
 app.listen(process.env.PORT, function() {
   console.log(`Server running at http://${process.env.IP}:${process.env.PORT}/`);
