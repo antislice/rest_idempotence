2146e2c6dc79dd1649196562dee3578c258fa402
Alyssa Kwan <me@alyssackwan.name>
2016-06-19 18:47:46 +0000

    integration tests

--- test/integration/test.js
+++ test/integration/test.js
@@ -25,13 +25,21 @@ describe('carts', function() {
       })
   });
   
-  it('should update purchase', function(done) {
-    var s = chai.request(server);
-    s.post('/carts').end(function(err, res) {
-      should.not.exist(err);
-      res.should.have.status(200);
-      res.body.id.should.equal(1);
-      res.body.purchase.should.equal(false);
+  describe('items', function() {
+
+    beforeEach(function(done) {
+      var s = chai.request(server);
+      s.post('/carts').end(function(err, res) {
+        should.not.exist(err);
+        res.should.have.status(200);
+        res.body.id.should.equal(1);
+        res.body.purchase.should.equal(false);
+        done();
+      });
+    });
+
+    it('should create items', function(done) {
+      var s = chai.request(server);
       s.post('/carts/1/items')
         .send({
           'productId': 'abc',
@@ -45,27 +53,12 @@ describe('carts', function() {
             'abc': 5
           });
           res.body.purchase.should.equal(false);
-          s.put('/carts/1/purchase').end(function(err, res) {
-            should.not.exist(err);
-            res.should.have.status(200);
-            res.body.id.should.equal(1);
-            res.body.items.should.deep.equal({
-              'abc': 5
-            })
-            res.body.purchase.should.equal(true);
-            done(); 
-          });
+          done();
         });
     });
-  });
   
-  it('should not update purchase twice', function(done) {
-    var s = chai.request(server);
-    s.post('/carts').end(function(err, res) {
-      should.not.exist(err);
-      res.should.have.status(200);
-      res.body.id.should.equal(1);
-      res.body.purchase.should.equal(false);
+    it('should create items cumulatively', function(done) {
+      var s = chai.request(server);
       s.post('/carts/1/items')
         .send({
           'productId': 'abc',
@@ -79,22 +72,95 @@ describe('carts', function() {
             'abc': 5
           });
           res.body.purchase.should.equal(false);
-          s.put('/carts/1/purchase').end(function(err, res) {
-            should.not.exist(err);
-            res.should.have.status(200);
-            res.body.id.should.equal(1);
-            res.body.items.should.deep.equal({
-              'abc': 5
+          s.post('/carts/1/items')
+            .send({
+              'productId': 'abc',
+              'quantity': 5
             })
-            res.body.purchase.should.equal(true);
-            s.put('/carts/1/purchase').end(function(err, res) {
-              should.exist(err);
-              res.should.have.status(400);
+            .end(function(err, res) {
+              should.not.exist(err);
+              res.should.have.status(200);
+              res.body.id.should.equal(1);
+              res.body.items.should.deep.equal({
+                'abc': 10
+              });
+              res.body.purchase.should.equal(false);
               done();
-            })
-          });
+            });
         });
     });
-  });
   
+    it('should not create items if etag missing');
+  
+    it('should not create items if etag none match');
+    
+    it('should update items');
+    
+    it('should not update items if etag missing');
+    
+    it('should not update items if etag none match');
+
+  })
+  
+  describe('purchase', function () {
+    
+    beforeEach(function(done) {
+      var s = chai.request(server);
+      s.post('/carts').end(function(err, res) {
+        should.not.exist(err);
+        res.should.have.status(200);
+        res.body.id.should.equal(1);
+        res.body.purchase.should.equal(false);
+        s.post('/carts/1/items')
+          .send({
+            'productId': 'abc',
+            'quantity': 5
+          })
+          .end(function(err, res) {
+            should.not.exist(err);
+            res.should.have.status(200);
+            res.body.id.should.equal(1);
+            res.body.items.should.deep.equal({
+              'abc': 5
+            });
+            res.body.purchase.should.equal(false);
+            done();
+          });
+      });
+    });
+    
+    it('should update purchase', function(done) {
+      var s = chai.request(server);
+      s.put('/carts/1/purchase').end(function(err, res) {
+        should.not.exist(err);
+        res.should.have.status(200);
+        res.body.id.should.equal(1);
+        res.body.items.should.deep.equal({
+          'abc': 5
+        })
+        res.body.purchase.should.equal(true);
+        done(); 
+      });
+    });
+    
+    it('should not update purchase twice', function(done) {
+      var s = chai.request(server);
+      s.put('/carts/1/purchase').end(function(err, res) {
+        should.not.exist(err);
+        res.should.have.status(200);
+        res.body.id.should.equal(1);
+        res.body.items.should.deep.equal({
+          'abc': 5
+        })
+        res.body.purchase.should.equal(true);
+        s.put('/carts/1/purchase').end(function(err, res) {
+          should.exist(err);
+          res.should.have.status(400);
+          done();
+        })
+      });
+    });
+
+  });
+
 });
