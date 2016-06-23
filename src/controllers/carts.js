eb95dd30a6a3cdcdf0d2414a15f2d1305bfc7d07
Alyssa Kwan <me@alyssackwan.name>
2016-06-23 23:31:08 +0000

    integration tests

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -45,6 +45,7 @@ exports.createItems = function(req, res) {
     return;
   }
   req.cart.addItems(productId, quantity);
+  res.status(201)
   res.set(cartView.header(req.cart));
   res.send(cartView.body(req.cart));
 };
